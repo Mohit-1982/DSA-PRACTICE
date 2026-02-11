16fa4f2d388355d745301a188309065c6ebe5f11
Mohit <mohitdagar2k08@gmail.com>
2026-02-11 16:36:27 +0530

    Add Kadane's Algorithm implementation
    Implemented Kadane's Algorithm for maximum subarray sum.

--- Arrays/KadanesAlgorithm.java
+++ Arrays/KadanesAlgorithm.java
@@ -21,3 +21,18 @@ Leetcode - 53
         return max;
     }
 }
+
+Optimal : 
+  class Solution {
+    public int maxSubArray(int[] nums) {
+        //Kadanes Algorithm
+        int maxi = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            maxi = Math.max(maxi,sum);
+            if(sum < 0) sum = 0;
+        }
+        return maxi;
+    }
+}
