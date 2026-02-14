4b59965e887e87a69a51dc3f5b189e56fee55526
Mohit <mohitdagar2k08@gmail.com>
2026-02-14 08:23:43 +0530

    Add optimal solution for buildArray method
    Implemented an optimal solution for building an array from permutations using in-place modifications.

--- Arrays/BuildArrayFromPermutations.java
+++ Arrays/BuildArrayFromPermutations.java
@@ -10,4 +10,15 @@ Leetcode - 1920
     }
 }
     Optimal : 
+      class Solution {
+    public int[] buildArray(int[] nums) {
+        for(int i = 0; i < nums.length; i++) {
+            nums[i] += 1000 * (nums[nums[i]] % 1000);
+        }
+        for(int i = 0; i < nums.length; i++) {
+            nums[i] /= 1000;
+        }
+        return nums;
+    }
+}
       
