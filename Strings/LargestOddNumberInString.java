9edde87e6c7182fe361b31afd66314382ca1e1a8
Mohit <mohitdagar2k08@gmail.com>
2026-02-21 22:19:41 +0530

    Create LargestOddNumberInString.java

---
+++ Strings/LargestOddNumberInString.java
@@ -0,0 +1,14 @@
+Leetcode - 1903
+  class Solution {
+    public String largestOddNumber(String num) {
+        for(int i = num.length() - 1; i >= 0; i--) {
+            int n = num.charAt(i) - '0';
+            if(n % 2 == 0) {
+                continue;
+            }else {
+                return num.substring(0, i + 1);
+            }
+        }
+        return "";
+    }
+}
